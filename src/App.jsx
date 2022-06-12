5252a7a4a6813326eda37e50a54c99a0fb2d8ab3
Kristian Fulkerson <kristianf451@gmail.com>
2022-06-11 20:28:20 -0700

    feat: eslint fix, router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
+import { Home } from './components';
 
 function App() {
   return (
     <div>
+      <CssBaseline />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+
       <h1>Test</h1>
     </div>
   );
