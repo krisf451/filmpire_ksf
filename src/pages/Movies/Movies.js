925c5e47e3ce889c7cb170b8a4b9ae5b1cc5218b
Kristian Fulkerson <kristianf451@gmail.com>
2022-06-19 17:24:36 -0700

    feat: search movies by search query working

--- src/pages/Movies/Movies.js
+++ src/pages/Movies/Movies.js
@@ -7,8 +7,14 @@ import { useGetMoviesQuery } from '../../redux/services/TMDB';
 
 function Movies() {
   const [page, setPage] = useState(1);
-  const { genreIdOrCategoryName } = useSelector((state) => state.currentGenreOrCategory);
-  const { data, error, isFetching } = useGetMoviesQuery({ genreIdOrCategoryName, page });
+  const { genreIdOrCategoryName, searchQuery } = useSelector(
+    (state) => state.currentGenreOrCategory,
+  );
+  const { data, error, isFetching } = useGetMoviesQuery({
+    genreIdOrCategoryName,
+    page,
+    searchQuery,
+  });
 
   if (isFetching) {
     return (
